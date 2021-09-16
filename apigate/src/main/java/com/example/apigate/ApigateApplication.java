9f31d4486b9d98702c029765cbb59af33f4d5740
udarasan <udarassanjeewa@gmail.com>
2021-09-16 10:37:23 +0530

    Initial commit

---
+++ apigate/src/main/java/com/example/apigate/ApigateApplication.java
@@ -0,0 +1,13 @@
+package com.example.apigate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigateApplication.class, args);
+	}
+
+}
---
+++ apigate/src/test/java/com/example/apigate/ApigateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apigate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
